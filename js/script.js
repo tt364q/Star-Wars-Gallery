9a092142b03b6b5bf4e6ea384843c70f4434b858
Taylor Torres <taylor23torres@gmail.com>
2022-07-09 17:18:56 -0500

    adds final js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
 const BASE_URL = 'https://swapi.dev/api/people/'
 
 // State Data
-let starwarsData;
+let starwarsData, starwarsDetail;
 
 // Cached Element References: Parts of user interfaces that we store in a variable so we can reference and make changes
 const $section = $('section');
@@ -25,13 +25,33 @@ function init(){
     getData();
 }
 
-function getData(){
+function getData(detailURL){
+
+    // declare a local variable to take whichever URL we need
+    let url;
+
+    if (detailURL === undefined) {
+        // we want all STARWARS people
+        url = BASE_URL
+    } else {
+        // we want single STARWARS person
+        url = detailURL;
+    }
     // Needs to fetch data using AJAX
-    $.ajax(BASE_URL).then(function(data){
+    $.ajax(url).then(function(data){
     // then take returned data and assignt it to global (state) variable
-    starwarsData = data;
-    // call render() to visualize it to the DOM
+    // we are gettin all the starwars data
+    if(detailURL === undefined){
+        starwarsData = data;
         render();
+    } else {
+        // we are getting single object
+        starwarsDetail = data;
+        // call render and tell the function that it needs to display a modal
+        render(true);
+    }
+        
+    // call render() to visualize it to the DOM
 
     }, function(error){
         console.log('Error:', error)
@@ -39,18 +59,36 @@ function getData(){
 }
 
 function handleClick(){
-    
+    getData(this.dataset.url)
 }
 
-function render(){
-    // map over the objects inside of starwarsData results array
-    // dynamicaly generate html for each element in the array
-    // add that html to collection element
-    const htmlArray = starwarsData.results.map(people => {
-        return `
-        <article class=" flex-ctr card">
-            <h3>${people.name}</h3>
-        </article>`;
-    });
-    $section.html(htmlArray);
+function render(showModal){
+    if(showModal === true){
+        //show the modal
+        // generate the html for the inner content for the modal
+        // call the modal function on the modal element
+        const $modalContent = $(`
+            <h5>${starwarsDetail.name}</h5>
+            <p>Birth Year: ${starwarsDetail.birth_year}</p>
+            <p>Height: ${starwarsDetail.height}</p>
+            <p>Hair Color: ${starwarsDetail.hair_color}</p>
+        `);
+
+        const $modal = $('#starmodal');
+        $modal.html($modalContent)
+        $modal.modal();
+
+    } else {
+        // map over the objects inside of starwarsData results array
+        // dynamicaly generate html for each element in the array
+        // add that html to collection element
+        
+        const htmlArray = starwarsData.results.map(people => {
+            return `
+            <article data-url="${people.url}"class=" flex-ctr card">
+                <h3>${people.name}</h3>
+            </article>`;
+        });
+        $section.html(htmlArray);
+    }
 }
